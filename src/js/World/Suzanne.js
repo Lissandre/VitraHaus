8a3dafac3e82ee52f116f2b7428d3aa3299ea8d6
Lissandre <lissandre.pasdeloup@hetic.net>
2020-12-09 11:18:10 +0100

    init from three template lite

---
+++ src/js/World/Suzanne.js
@@ -0,0 +1,35 @@
+import { Object3D } from 'three'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+import SuzanneModel from '@models/suzanne.glb'
+
+export default class Suzanne {
+  constructor(options) {
+    // Options
+    this.time = options.time
+
+    // Set up
+    const dracoLoader = new DRACOLoader()
+    dracoLoader.setDecoderPath('./draco/')
+    dracoLoader.setDecoderConfig({ type: 'js' })
+
+    this.gltfLoader = new GLTFLoader()
+    this.gltfLoader.setDRACOLoader(dracoLoader)
+    this.container = new Object3D()
+
+    this.createSuzanne()
+  }
+  createSuzanne() {
+    this.gltfLoader.load(SuzanneModel, (loaded) => {
+      this.suzanne = loaded.scene
+      this.container.add(this.suzanne)
+      this.setMovement()
+    })
+  }
+  setMovement() {
+    this.time.on('tick', () => {
+      this.suzanne.rotation.y += 0.005
+    })
+  }
+}
---
+++ src/js/World/index.js
@@ -0,0 +1,43 @@
+import { Object3D } from 'three'
+
+import AmbientLightSource from './AmbientLight'
+import PointLightSource from './PointLight'
+import Suzanne from './Suzanne'
+
+export default class World {
+  constructor(options) {
+    // Set options
+    this.time = options.time
+    this.debug = options.debug
+
+    // Set up
+    this.container = new Object3D()
+
+    if (this.debug) {
+      this.debugFolder = this.debug.addFolder('World')
+      this.debugFolder.open()
+    }
+
+    this.setAmbientLight()
+    this.setPointLight()
+    this.setSuzanne()
+  }
+  setAmbientLight() {
+    this.light = new AmbientLightSource({
+      debug: this.debugFolder,
+    })
+    this.container.add(this.light.container)
+  }
+  setPointLight() {
+    this.light = new PointLightSource({
+      debug: this.debugFolder,
+    })
+    this.container.add(this.light.container)
+  }
+  setSuzanne() {
+    this.suzanne = new Suzanne({
+      time: this.time,
+    })
+    this.container.add(this.suzanne.container)
+  }
+}
