17a03596c434c862eccb013bfbec96acdc996c00
Lissandre <lissandre.pasdeloup@hetic.net>
2020-12-17 12:12:08 +0100

    animation on infos

--- src/js/Infos.js
+++ src/js/Infos.js
@@ -1,5 +1,6 @@
 import EventEmitter from '@tools/EventEmitter'
 import { Color, Raycaster, Vector2 } from 'three'
+import anime from 'animejs'
 import data from '@/data.json'
 
 export default class Controls extends EventEmitter {
@@ -13,6 +14,7 @@ export default class Controls extends EventEmitter {
 
     // Set up
     this.infosDOM = document.querySelector('.infos')
+    this.nameDOM = document.querySelector('h1.name')
     this.titleDOM = this.infosDOM.querySelector('h2.title')
     this.descriptionDOM = this.infosDOM.querySelector('p.description')
     this.linkDOM = this.infosDOM.querySelector('a.action')
@@ -20,6 +22,7 @@ export default class Controls extends EventEmitter {
     this.mouse = {}
     this.raycaster = new Raycaster()
     this.direction = new Vector2()
+    this.lastindex = null
 
     this.mouseMove()
   }
@@ -34,6 +37,8 @@ export default class Controls extends EventEmitter {
         house.traverse((child) => {
           if (child.isMesh) {
             this.objects.push(child)
+            child.material.emissiveIntensity = 0
+
           }
         })
       })
@@ -41,32 +46,104 @@ export default class Controls extends EventEmitter {
       this.intersects = this.raycaster.intersectObjects(this.objects)
 
       if (this.intersects.length > 0) {
-        this.setInfos(
+        this.setName(
           this.houses.indexOf(this.intersects[0].object.parent.parent)
         )
-        this.houses.forEach((house) => {
-          house.traverse((child) => {
-            if (child.isMesh) {
-              child.material.emissiveIntensity = 0
-            }
-          })
-        })
         this.intersects[0].object.parent.traverse((child) => {
           if (child.isMesh) {
             child.material.emissiveIntensity = 1
             child.material.emissive = new Color(0xff0000)
           }
         })
+      } else {
+        this.removeName()
       }
     })
   }
-  setInfos(index) {
-    if (this.infosDOM.classList.contains('hidden')) {
-      this.infosDOM.classList.remove('hidden')
+  setName(index) {
+    if(this.lastindex != index){
+      this.lastindex = index
+      this.nameDOM.innerHTML = data[index].title
+      anime({
+        targets: this.nameDOM,
+        opacity: [
+          { value: 0, duration: 0 },
+          { value: 1, duration: 250 },
+        ],
+        rotate: [
+          { value: 6, duration: 0 },
+          { value: 0, duration: 520 },
+        ],
+        translateY: [
+          { value: '-20%', duration: 0 },
+          { value: '-50%', duration: 520 },
+        ],
+        easing: 'easeOutQuad',
+        duration: 320,
+      })
     }
+  }
+  removeName() {
+    if(this.lastindex){
+      this.lastindex = null
+      anime({
+        targets: this.nameDOM,
+        opacity: [
+          { value: 1, duration: 0 },
+          { value: 0, duration: 250 },
+        ],
+        rotate: [
+          { value: 0, duration: 0 },
+          { value: -6, duration: 520 },
+        ],
+        translateY: [
+          { value: '-50%', duration: 0 },
+          { value: '-70%', duration: 520 },
+        ],
+        easing: 'easeOutQuad',
+        duration: 320,
+      })
+    }
+  }
+  setInfos(index) {
     this.titleDOM.innerHTML = data[index].title
-    // this.titleDOM.innerHTML = data[index].title
-    // this.descriptionDOM.innerHTML = data[index].description
-    // this.linkDOM.href = data[index].url
+    this.descriptionDOM.innerHTML = data[index].description
+    this.linkDOM.href = data[index].url
+    anime({
+      targets: this.infosDOM,
+      opacity: [
+        { value: 0, duration: 0 },
+        { value: 1, duration: 250 },
+      ],
+      rotate: [
+        { value: 6, duration: 0 },
+        { value: 0, duration: 520 },
+      ],
+      translateY: [
+        { value: '-20%', duration: 0 },
+        { value: '-50%', duration: 520 },
+      ],
+      easing: 'easeOutQuad',
+      duration: 320,
+    })
+  }
+  removeInfos() {
+    anime({
+      targets: this.infosDOM,
+      opacity: [
+        { value: 0, duration: 0 },
+        { value: 1, duration: 250 },
+      ],
+      rotate: [
+        { value: 6, duration: 0 },
+        { value: 0, duration: 520 },
+      ],
+      translateY: [
+        { value: '-20%', duration: 0 },
+        { value: '-50%', duration: 520 },
+      ],
+      easing: 'easeOutQuad',
+      duration: 320,
+    })
   }
 }
